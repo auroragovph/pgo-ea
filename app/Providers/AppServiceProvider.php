41c0d192b43fbb4b419292df6a18e52d16120091
Jimwell Parinas <jp.pagapulan@gmail.com>
2021-10-04 11:19:28 +0800

    update to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Request $request)
     {
-        //
-        if ($request->server->has('HTTP_X_ORIGINAL_HOST')) {
-            $this->app['url']->forceRootUrl($request->server->get('HTTP_X_FORWARDED_PROTO').'://'.$request->server->get('HTTP_X_ORIGINAL_HOST'));
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
         }
     }
 }
