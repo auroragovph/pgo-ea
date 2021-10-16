697e4e379f812e22a93b47458a9890d7a9bebced
Jimwell Parinas <jp.pagapulan@gmail.com>
2021-10-16 13:50:13 +0800

    add dev controller

---
+++ app/Http/Controllers/DevController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Applicant;
+use Illuminate\Http\Request;
+
+class DevController extends Controller
+{
+    public function reds()
+    {
+        $applicants = Applicant::get();
+
+        foreach($applicants->groupBy('name') as $groups){
+
+            if($groups->count() !== 1){
+
+                // removing the last item
+                $groups->pop();
+                $reds = collect($groups->all());
+                $red_ids = $reds->pluck('id');
+
+                // deleting the applicant
+                $del = Applicant::whereIn('id', $red_ids)->delete();
+            }
+            continue;
+        }
+    }
+}
